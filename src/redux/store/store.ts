ebbed159bcaef0d36e5e119468e333c553f6675b
tsheporamantso <develop@molotovcocktail.tv>
2024-12-31 18:34:39 +0200

    Add createJobPost reducer to Redux store configuration

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createJobPostReducer from "../CreateJobPostSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    createJobPost: createJobPostReducer,
+  },
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
