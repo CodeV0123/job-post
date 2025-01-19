e7283ecad045317386253975b200e288d2cfa8bd
tsheporamantso <develop@molotovcocktail.tv>
2025-01-19 12:10:33 +0200

    Refactor CreateJob component to move step indicator above job title for improved layout

--- src/components/CreateJob.tsx
+++ src/components/CreateJob.tsx
@@ -131,10 +131,10 @@ const CreateJobPost: React.FC = () => {
 
   return (
     <div className="max-w-3xl mx-auto mt-10 p-6 bg-white shadow-md rounded-md">
+      <span className="italic text- text-gray-400 text-base">
+        {isEnglish ? "[First Step]:" : "[Erster Schritt]:"}{" "}
+      </span>
       <h1 className="text-2xl text-center font-semibold text-gray-700 mb-2">
-        <span className="italic text-gray-400 text-base">
-          {isEnglish ? "[First Step]:" : "[Erster Schritt]:"}{" "}
-        </span>
         {isEnglish ? "Create Job Post" : "Stellenanzeige Erstellen"}
       </h1>
       {/* Language Toggle */}
