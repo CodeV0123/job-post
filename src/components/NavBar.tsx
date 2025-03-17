4f6adc671fb0feba41c5ce6672c819f0213de42d
tsheporamantso <develop@molotovcocktail.tv>
2025-03-17 14:56:47 +0200

    Update NavBar component to adjust language toggle text size

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <nav className="flex justify-between items-center bg-[#fff] p-3 ">
       <img className="h-[50px] w-[70px]" src={navlogo} alt="navbar image" />
-      <h1 className="font-normal text-[#5d5c61] uppercase text-center">
+      <h1 className="font-normal text-[#5d5c61] text-sm uppercase text-center">
         toggle
         <br /> language
         <ToggleLanguage />
