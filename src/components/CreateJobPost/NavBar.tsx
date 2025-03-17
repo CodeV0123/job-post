bd34ae91e777b63dcf8f61793b6b05b8b59e021b
tsheporamantso <develop@molotovcocktail.tv>
2025-03-17 19:24:35 +0200

    Add language toggle component to NavBar and adjust text size

--- src/components/CreateJobPost/NavBar.tsx
+++ src/components/CreateJobPost/NavBar.tsx
@@ -1,12 +1,14 @@
 import navlogo from "./assets/cjp_navlogo.png";
+import ToggleLanguage from "../ToggleLanguage/ToggleLanguage";
 
 const NavBar = () => {
   return (
     <nav className="flex justify-between items-center bg-[#fff] p-3 ">
       <img className="h-[50px] w-[70px]" src={navlogo} alt="navbar image" />
-      <h1 className="font-normal text-[#5d5c61] uppercase text-center">
+      <h1 className="text-[#5d5c61] text-sm uppercase text-center">
         toggle
         <br /> language
+        <ToggleLanguage />
       </h1>
     </nav>
   );
