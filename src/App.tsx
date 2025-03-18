969efdb065db302f4f6839180b3b234904571f4a
tsheporamantso <develop@molotovcocktail.tv>
2025-03-18 09:16:13 +0200

    Add GenerateImage route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 // import CreateJobPost from "./components/CreateJobPost/CreateJob";
 import CreateJob from "./components/CreateJobPost/CreateJob";
+import GenerateImage from "./components/GenerateImage/GenerateImage";
 function App() {
   return (
     <React.Fragment>
       <Routes>
         <Route path="/" element={<CreateJob />} />
+        <Route path="/generate-image" element={<GenerateImage />} />
       </Routes>
       {/* <CreateJobPost /> */}
     </React.Fragment>
