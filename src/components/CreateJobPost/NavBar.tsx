938ab398ef0b4acd81d129be11fb82f87ca0ed45
tsheporamantso <develop@molotovcocktail.tv>
2025-03-18 13:36:57 +0200

    Update ToggleLanguage component in NavBar with color scheme and text color props

--- src/components/CreateJobPost/NavBar.tsx
+++ src/components/CreateJobPost/NavBar.tsx
@@ -5,7 +5,7 @@ const NavBar = () => {
   return (
     <nav className="flex justify-between items-center bg-[#fff] p-3 ">
       <img className="h-[50px] w-[70px]" src={navlogo} alt="navbar image" />
-      <ToggleLanguage />
+      <ToggleLanguage colorScheme="pink" textColor="text-gray-700" />
     </nav>
   );
 };
