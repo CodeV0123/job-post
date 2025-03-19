807e8f1e535fdcefdff5493a03cc4b4a8d681af6
tsheporamantso <develop@molotovcocktail.tv>
2025-03-19 11:39:07 +0200

    Add navigation functionality to NavBar components for improved user experience

--- src/components/CreateJobPost/NavBar.tsx
+++ src/components/CreateJobPost/NavBar.tsx
@@ -1,10 +1,17 @@
 import navlogo from "./assets/cjp_navlogo.png";
 import ToggleLanguage from "../ToggleLanguage/ToggleLanguage";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const navigate = useNavigate();
   return (
     <nav className="flex justify-between items-center bg-[#fff] p-3 ">
-      <img className="h-[50px] w-[70px]" src={navlogo} alt="navbar image" />
+      <img
+        onClick={() => navigate("/")}
+        className="h-[50px] w-[70px] cursor-pointer"
+        src={navlogo}
+        alt="navbar image"
+      />
       <ToggleLanguage colorScheme="pink" textColor="text-gray-700" />
     </nav>
   );
--- src/components/GenerateImage/NavBar.tsx
+++ src/components/GenerateImage/NavBar.tsx
@@ -1,9 +1,16 @@
 import navlogo from "./assets/Vector.png";
+import { useNavigate } from "react-router-dom";
 
 const NavBar = () => {
+  const navigate = useNavigate();
   return (
     <nav className="flex justify-between items-center bg-[#fff] p-3 ">
-      <img className="h-[50px] w-[70px]" src={navlogo} alt="navbar image" />
+      <img
+        onClick={() => navigate("/")}
+        className="h-[50px] w-[70px] cursor-pointer"
+        src={navlogo}
+        alt="navbar image"
+      />
     </nav>
   );
 };
