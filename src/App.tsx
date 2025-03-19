cc8185ba7c16fade73937014b14488cb7784583c
tsheporamantso <develop@molotovcocktail.tv>
2025-03-19 11:42:46 +0200

    Add route for GenerateVideo component in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 // import CreateJobPost from "./components/CreateJobPost/CreateJob";
 import CreateJob from "./components/CreateJobPost/CreateJob";
 import GenerateImage from "./components/GenerateImage/GenerateImage";
+import GenerateVideo from "./components/GenerateVideo/GenerateVideo";
 function App() {
   return (
     <React.Fragment>
       <Routes>
         <Route path="/" element={<CreateJob />} />
         <Route path="/generate-image" element={<GenerateImage />} />
+        <Route path="/generate-video" element={<GenerateVideo />} />
       </Routes>
       {/* <CreateJobPost /> */}
     </React.Fragment>
