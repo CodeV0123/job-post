afbd20dad45908e7886ab9df6cb4cfc32974eec2
tsheporamantso <develop@molotovcocktail.tv>
2025-03-25 21:25:40 +0200

    Enhance GeneratedVideo component to display taglines overlay and improve video layout

--- src/components/GenerateVideo/GeneratedVideo.tsx
+++ src/components/GenerateVideo/GeneratedVideo.tsx
@@ -1,18 +1,54 @@
-// import { generateVideo } from "../../redux/slice/GenerateVideoSlice";
 import { RootState } from "../../redux/store/store";
 import { useSelector } from "react-redux";
+import { selectTaglines } from "../../redux/slice/CreateJobSlice"; // Import memoized selector
 
 const GeneratedVideo = () => {
   const { videoResponse } = useSelector(
     (state: RootState) => state.generateVideo
   );
+  const taglines = useSelector(selectTaglines); // Use memoized selector
+
+  console.log("Taglines:", taglines);
+
   return (
     <div className="bg-white rounded-lg shadow-lg p-8 max-w-xl w-full mx-4 relative overflow-hidden">
-      {videoResponse && videoResponse.video_path ? (
-        <video src={videoResponse.video_path} controls></video>
-      ) : (
-        <p>No video available</p>
-      )}
+      <div className="relative">
+        {/* Video */}
+        {videoResponse?.video_path ? (
+          <video src={videoResponse.video_path} controls className="w-full" />
+        ) : (
+          <p>No video available</p>
+        )}
+
+        {/* Taglines Overlay */}
+        <div className="absolute bottom-0 left-0 w-full bg-red-600 text-white p-4">
+          <ul className="list-none">
+            {taglines.length > 0 ? (
+              taglines.map((tagline, index) => (
+                <li key={index} className="text-lg font-medium flex ">
+                  <svg
+                    className="w-10 h-10 text-white mr-2"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2.5"
+                      d="M5 13l4 4L19 7"
+                    />
+                  </svg>
+                  {tagline}
+                </li>
+              ))
+            ) : (
+              <p>No taglines available</p>
+            )}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
