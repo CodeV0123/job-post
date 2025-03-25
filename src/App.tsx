7e7fd2f63233081252c3410a676efbba49150f6a
tsheporamantso <develop@molotovcocktail.tv>
2025-03-25 14:54:50 +0200

    Add route for CreatedJob component in App and connect Redux state

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store/store";
 // import CreateJobPost from "./components/CreateJobPost/CreateJob";
 import CreateJob from "./components/CreateJobPost/CreateJob";
 import GenerateImage from "./components/GenerateImage/GenerateImage";
 import GenerateVideo from "./components/GenerateVideo/GenerateVideo";
+import CreatedJob from "./components/CreatedJob/CreatedJob";
+
 function App() {
+  const { jobPost, language } = useSelector(
+    (state: RootState) => state.createJob
+  );
   return (
     <React.Fragment>
       <Routes>
         <Route path="/" element={<CreateJob />} />
         <Route path="/generate-image" element={<GenerateImage />} />
         <Route path="/generate-video" element={<GenerateVideo />} />
+        <Route
+          path="/created-job"
+          element={<CreatedJob jobPost={jobPost || {}} language={language} />}
+        />
       </Routes>
       {/* <CreateJobPost /> */}
     </React.Fragment>
