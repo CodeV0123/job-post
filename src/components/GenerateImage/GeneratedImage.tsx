c469ca6ee923df39fb468a12b80446ef71d1cf64
tsheporamantso <develop@molotovcocktail.tv>
2025-03-25 22:07:46 +0200

    Add GeneratedImage component to display generated images from state

---
+++ src/components/GenerateImage/GeneratedImage.tsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/store/store";
+
+export const GeneratedImage = () => {
+  const { images, status } = useSelector(
+    (state: RootState) => state.generateImage
+  );
+  if (status !== "succeeded" || images.length === 0) return null;
+  return (
+    <div className="mt-6 grid grid-cols-2 gap-4 bg-white rounded-lg shadow-lg p-6 max-w-xl w-full mx-4 relative overflow-hidden">
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={`data:image/png;base64,${image}`}
+          alt={`Generated ${index + 1}`}
+          className="w-[400px] h-[200px] rounded-lg shadow-md"
+        />
+      ))}
+    </div>
+  );
+};
